102af79809a1f8a9161131f67e0cc53170525329
Secure Diana - Elena <tablidiana@gmail.com>
2025-05-26 19:08:17 +0300

    change hardcoded port

--- album-shop-backend/server.js
+++ album-shop-backend/server.js
@@ -47,7 +47,7 @@ app.get("/", (req, res) => {
     res.send("Album Shop RESTful API is running!");
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     log(`Server successfully started on port ${PORT}`);
     log('Available endpoints:');
